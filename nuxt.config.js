1af7325fb5f00d33983d68d6edd97a6780f77566
resessh <info@resessh.net>
2021-10-13 03:15:45 +0900

    add gtm-module and composition-api

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,16 +18,23 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ["@/plugins/gtm.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: ["@nuxtjs/composition-api/module"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    [
+      "@nuxtjs/gtm",
+      {
+        enabled: true
+      }
+    ]
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
